bf327f774af7a7f936faf2588a7f9305b288a979
Tom McLean <tom@vegaprotocol.io>
2023-12-27 21:03:14 +0800

    feat: Improving PoW logic

--- tests/test_client.py
+++ tests/test_client.py
@@ -84,8 +84,22 @@ def test_basic_signing(servicers_and_port):
 
 
 def test_client_pubkey(servicers_and_port):
-    _, port, _, _ = servicers_and_port
+    server, port, core_servicer, trading_data_servicer = servicers_and_port
 
+    def LastBlockHeight(request, context):
+        return core_proto.LastBlockHeightResponse(
+            height=245,
+            hash="2FB2146FC01F21D358323174BAA230E7DE61C0F150B7FBC415C896B0C23E50FF",
+            chain_id="4",
+            spam_pow_difficulty=1,
+            spam_pow_number_of_past_blocks=100,
+            spam_pow_number_of_tx_per_block=100,
+        )
+
+    core_servicer.LastBlockHeight = LastBlockHeight
+
+    add_TradingDataServiceServicer_v2_to_server(trading_data_servicer, server)
+    add_CoreServiceServicer_to_server(core_servicer, server)
     client = Client(
         mnemonic=(
             "fancy basket install citizen purchase flush raven valid pottery short pony"
